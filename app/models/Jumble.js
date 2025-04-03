b0b8d1e69404a1a76f53a6277442fc8d32d762c8
AdamRB208 <adamrborup@gmail.com>
2025-04-03 12:11:38 -0600

    created Jumble model and simple template

---
+++ app/models/Jumble.js
@@ -0,0 +1,20 @@
+import { generateId } from "../utils/GenerateId.js";
+
+
+export class Jumble {
+  constructor(data) {
+    this.id = generateId()
+    this.name = data.name
+    this.body = data.body
+    this.fastestTime = null
+    this.startTime = null
+    this.endTime = null
+  }
+
+  getListTemplate() {
+    return /*html*/
+    `
+      <li>${this.name}</li>
+    `
+  }
+}
